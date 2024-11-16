d6f8ba5ea282ccc9c59beb8842b5d07abbda2877
GrozniyMax <AzmEcmTzar@mail.ru>
2024-11-16 16:33:33 +0300

    rewrited javadoc

--- src/main/java/com/maxim/Localize.java
+++ src/main/java/com/maxim/Localize.java
@@ -6,28 +6,30 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Данной аннотацией надо помечать все поля подлежащие локализации
+ * Annotation that is made to mark classes and fields that should be localized
+ * <strong>All classes that hava to be localized should be marked with this annotations</strong>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.TYPE})
 public @interface Localize {
 
     /**
-     * <h3>Для класса</h3><br>
-     * Значение обозначает путь к {@link java.util.ResourceBundle} в ресурсах приложения (разделитель .)
-     * Если не указано значение, то будет использовано полное имя класса
+     * <h3>For class</h3><br>
+     * {@code value()} is interpreted as path to {@link java.util.ResourceBundle} in resources folder/package
+     * If value is absent, full name of class (e.g. {@code com.smth.smth.etc}) will be used
      * <br>
      *
-     * <h3>Для Enum</h3>
+     * <h3>For Enum</h3>
+     * All instances of enum class will be localized.<br>
+     * <strong>Field value for enum must have prefix of lower cased instance name</strong>
      *
-     * <h3>Для поля</h3><br>
-     * <strong>Для строковых полей</strong><br>
-     * Значение обозначает имя ключ в {@link java.util.Properties} файле
-     * Если не указано значение, то будет использоваться имя переменной,
-     * где "CamelCase" будет преобразован в "camel.case"
+     * <h3>For field</h3><br>
+     * <strong>For String fields</strong><br>
+     * Key name in required {@link java.util.Properties} file
+     * If no value is provided, will be used name of the field, which will be changed to properties-like syntax
+     *  (e.g. {@code CamelCaseName} to {@code camel.case.name})
      * <br>
-     * <strong>Для полей сложных объектов</strong><br>
-     * Значение указывать не обязательно<br>
+     * <strong>No value needed if field has complex type, that should be also localized</strong><br>
      */
     String value() default "";
 
